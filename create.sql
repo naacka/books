1ca8573c890120dc2cb437df1175613e1b5a83b7
nicole <nyrnack@gmail.com>
2019-03-10 11:09:45 -0400

    Initial commit

---
+++ create.sql
@@ -0,0 +1,13 @@
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    isbn VARCHAR NOT NULL,
+    title VARCHAR NOT NULL,
+    author VARCHAR NOT NULL,
+    year VARCHAR NOT NULL
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR NOT NULL,
+    password VARCHAR NOT NULL
+);
